5640f80887d08941ad227d109eca0e3933cbe0a4
orbital <orbital@holgerines.de>
2023-05-23 19:46:24 -0700

    Change seeds in computer play

--- src/play.cpp
+++ src/play.cpp
@@ -5,6 +5,7 @@
 #include <folly/experimental/coro/Task.h>
 
 #include <algorithm>
+#include <random>
 #include <ranges>
 
 #include "mcts.hpp"
@@ -14,9 +15,10 @@ namespace views = std::ranges::views;
 folly::coro::Task<Player> computer_play_single(const Board& board,
                                                std::shared_ptr<MCTSPolicy> policy1,
                                                std::shared_ptr<MCTSPolicy> policy2,
+                                               std::uint32_t seed,
                                                ComputerPlayOptions const& opts) {
-    MCTS mcts1{policy1, board};
-    MCTS mcts2{policy2, board};
+    MCTS mcts1{policy1, board, {.seed = opts.seed}};
+    MCTS mcts2{policy2, board, {.seed = opts.seed}};
 
     while (true) {
         co_await mcts1.sample(opts.samples);
@@ -38,8 +40,11 @@ folly::coro::Task<Player> computer_play_single(const Board& board,
 folly::coro::Task<double> computer_play(const Board& board, std::shared_ptr<MCTSPolicy> policy1,
                                         std::shared_ptr<MCTSPolicy> policy2, int games,
                                         ComputerPlayOptions const& opts) {
+    std::mt19937_64 twister{opts.seed};
+    std::uniform_int_distribution<std::uint32_t> dist;
+
     auto game_tasks = views::iota(0, games) | views::transform([&](int) {
-                          return computer_play_single(board, policy1, policy2, opts);
+                          return computer_play_single(board, policy1, policy2, dist(twister), opts);
                       });
 
     auto results = co_await folly::coro::collectAllRange(game_tasks);
