60e7cb97b84ffee58557a8f2b1d4334d1a9254cb
Thorben Troebst <thorben.troebst@gmail.com>
2024-02-16 22:26:45 -0800

    Fix missing model parameter in training script

--- scripts/train_generation.sh
+++ scripts/train_generation.sh
@@ -2,4 +2,4 @@
 
 python3 training.py ../data ../models $1 > ../models/stats_$1.txt
 trtexec --onnx=../models/model_$1.onnx --saveEngine=../models/model_$1.trt
-../build/deep_ww -snapshots ../data/snapshots_$1.csv -games 5000
+../build/deep_ww -model ../models/model_$1.trt -snapshots ../data/snapshots_$1.csv -games 5000
