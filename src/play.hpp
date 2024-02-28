936e97f7526d6c5bf876085fa7158050dffbe4dd
Thorben Troebst <thorben.troebst@gmail.com>
2024-02-27 20:38:06 -0800

    Raise self-play temperature

--- src/play.hpp
+++ src/play.hpp
@@ -24,7 +24,7 @@ struct ComputerPlayOptions {
     int max_parallel_games = 128;
     int max_parallel_samples = 16;
     int move_limit = 100;
-    double temperature = 0.2;
+    double temperature = 1;
     std::uint32_t seed = 42;
     CompletionCallback on_complete = [](MCTS const&, int) {};
 };
